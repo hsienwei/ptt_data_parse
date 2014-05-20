9c1bfe4f7e83b460e68b2989eb03036bc2dcd710
HsienWei Hsiang <george.hsiang@udngroup.com.tw>
2014-05-20 16:51:45 +0800

    port修改

--- ptt_data_node/app.js
+++ ptt_data_node/app.js
@@ -14,7 +14,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(partials());
