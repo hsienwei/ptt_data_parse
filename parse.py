3baae2ca238182860524902801e21ad077442b42
HsienWei Hsiang <george.hsiang@udngroup.com.tw>
2014-05-30 10:40:09 +0800

    no message

--- parse.py
+++ parse.py
@@ -10,13 +10,19 @@ def update_board_list(list):
 	for board_name in list:
 		db.board_list.insert({'board':board_name})
 
-if __name__ == "__main__":
+def update_board_data():
 	board_list = ['Gossiping', 'beauty', 'joke', 'StupidClown', 'sex']
 
 	parser = ptt_parser.PttWebParser()
-	parser.board_parse('Gossiping', 24)
-	parser.board_parse('beauty', 24)
-	parser.board_parse('joke', 24)
-	parser.board_parse('StupidClown', 24)
+	# parser.board_parse('Gossiping', 24)
+	# parser.board_parse('beauty', 24)
+	# parser.board_parse('joke', 24)
+	# parser.board_parse('StupidClown', 24)
 	parser.board_parse('sex', 24)
-	update_board_list(board_list)
+	parser.board_parse('PublicIssue', 24)
+	parser.board_parse('HatePolitics', 24)
+	update_board_list(board_list)		
+
+if __name__ == "__main__":
+
+	update_board_data()
